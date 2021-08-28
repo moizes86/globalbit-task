3f230821bae783886a6c7632fb090ceec8ea58fe
moizes86 <moshe.mn86@gmail.com>
2021-08-28 23:56:02 +0300

    Added links in display-users

--- client/src/Components/DisplayUsers.js
+++ client/src/Components/DisplayUsers.js
@@ -17,26 +17,35 @@ export default function DisplayUsers() {
       {loading ? (
         <Spinner />
       ) : data ? (
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((user, i) => (
-              <tr key={user.email}>
-                <th scope="row">{i + 1}</th>
-                <td>{user.firstname}</td>
-                <td>{user.lastname}</td>
-                <td>{user.email}</td>
+        <>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Email</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {data.map((user, i) => (
+                <tr key={user.email}>
+                  <th scope="row">{i + 1}</th>
+                  <td>{user.firstname}</td>
+                  <td>{user.lastname}</td>
+                  <td>{user.email}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div className="mt-3 mr-md-5 d-flex justify-content-center">
+            <Link to="register" className="mr-3">
+              Register
+            </Link>
+            <Link to="login">Login</Link>
+          </div>
+        </>
       ) : (
         <>
           {error} - please <Link to="/login"> login </Link> and verify your account
